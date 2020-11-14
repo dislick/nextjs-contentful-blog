688d6739f0b94066c973ed5e6d4b02eb791ecae0
Patrick Muff <muff.pa@gmail.com>
2020-11-14 18:48:09 +0100

    Everything

---
+++ pages/articles/[slug].js
@@ -0,0 +1,57 @@
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import Image from "next/image";
+
+let client = require("contentful").createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function getStaticPaths() {
+  let data = await client.getEntries({
+    content_type: "article",
+  });
+
+  return {
+    paths: data.items.map((item) => ({
+      params: { slug: item.fields.slug },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  let data = await client.getEntries({
+    content_type: "article",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      article: data.items[0],
+    },
+  };
+}
+
+export default function Article({ article }) {
+  console.log(article);
+
+  return (
+    <div>
+      <h1>{article.fields.title}</h1>
+      <div>
+        {documentToReactComponents(article.fields.content, {
+          renderNode: {
+            [BLOCKS.EMBEDDED_ASSET]: (node) => (
+              <Image
+                src={"https:" + node.data.target.fields.file.url}
+                width={node.data.target.fields.file.details.image.width}
+                height={node.data.target.fields.file.details.image.height}
+              />
+            ),
+          },
+        })}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,44 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+let client = require("contentful").createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+});
+
+export async function getStaticProps() {
+  let data = await client.getEntries({
+    content_type: "article",
+  });
+
+  return {
+    props: {
+      articles: data.items,
+    },
+  };
+}
+
+export default function Home({ articles }) {
+  console.log(articles);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Next.js + Contentful</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.sys.id}>
+              <Link href={"/articles/" + article.fields.slug}>
+                <a>{article.fields.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
